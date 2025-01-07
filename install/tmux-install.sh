ad87ee4307074d75a56847b19b96ed3ff8be118b
Iskander <iskandervdh@gmail.com>
2025-01-08 00:19:17 +0100

    Enhance tmux installation script to manage submodules and backup existing configuration

--- install/tmux-install.sh
+++ install/tmux-install.sh
@@ -9,3 +9,16 @@ fi
 
 # Create symbolic link to .tmux.conf
 ln -s $DOTFILES/.tmux.conf ~/.tmux.conf
+
+# Install submodules and update them
+git submodule init && git submodule update
+
+# Move .tmux to .tmux.bak if it exists
+if [ -d ~/.tmux ]; then
+    mv ~/.tmux ~/.tmux.bak
+fi
+
+# Create symbolic link to .tmux
+ln -s $DOTFILES/.tmux ~/.tmux
+
+bash ~/.tmux/plugins/tpm/bin/install_plugins
