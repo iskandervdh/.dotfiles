d5359acab3a55f2388abffec9f9b84a35210ae32
Iskander <iskandervdh@gmail.com>
2025-05-02 12:07:05 +0200

    🔧 Add update i3 config and add i3 status config

--- install/i3-install.sh
+++ install/i3-install.sh
@@ -10,6 +10,16 @@ fi
 # Create symbolic link to i3 config
 ln -s $DOTFILES/.config/i3/config ~/.config/i3/config
 
+
+# Move i3status config to config.bak if it exists
+if [ -f ~/.config/i3status/config ]; then
+    mv ~/.config/i3status/config ~/.config/i3status/config.bak
+fi
+
+# Create symbolic link to i3status config
+ln -s $DOTFILES/.config/i3status/config ~/.config/i3status/config
+
+
 # Move gtk-3.0 settings.ini to settings.ini.bak if it exists
 if [ -f ~/.config/gtk-3.0/settings.ini ]; then
     mv ~/.config/gtk-3.0/settings.ini ~/.config/gtk-3.0/settings.ini.bak
@@ -17,6 +27,7 @@ fi
 
 # Create symbolic link to gtk-3.0 settings.ini
 ln -s $DOTFILES/.config/gtk-3.0/settings.ini ~/.config/gtk-3.0/settings.ini
+
 
 # Move rofi config.rasi to config.rasi.bak if it exists
 if [ -f ~/.config/rofi/config.rasi ]; then
