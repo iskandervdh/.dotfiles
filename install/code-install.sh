5c7d2e63d433cf60bac7c4146ea69a2e53fea657
Iskander <iskandervdh@gmail.com>
2025-09-01 09:48:36 +0200

    🔧 Add keybindings for merge conflict navigation and update installation script

--- install/code-install.sh
+++ install/code-install.sh
@@ -7,5 +7,13 @@ if [ -f ~/.config/Code/User/settings.json ]; then
     mv ~/.config/Code/User/settings.json ~/.config/Code/User/settings.json.bak
 fi
 
+# Move vscode keybindings.json to keybindings.json.bak if it exists
+if [ -f ~/.config/Code/User/keybindings.json ]; then
+    mv ~/.config/Code/User/keybindings.json ~/.config/Code/User/keybindings.json.bak
+fi
+
 # Create symbolic link to vscode settings.json
 ln -s $DOTFILES/.config/Code/User/settings.json ~/.config/Code/User/settings.json
+
+# Create symbolic link to vscode keybindings.json
+ln -s $DOTFILES/.config/Code/User/keybindings.json ~/.config/Code/User/keybindings.json
